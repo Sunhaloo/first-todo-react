e489519a8d58158256f44994638f060da91e3938
Sunhaloo <azmaan151003@gmail.com>
2025-11-12 16:32:24 +0400

    Update: Started working on the main "homepage"

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,11 +1,25 @@
+// import the `useEffect` and `useState` hooks from 'react'
+import { useEffect, useState } from "react";
+
 // import the `useAuth` function from `AuthContext.jsx`
 import { useAuth } from "../contexts/AuthContext";
-import { Button } from "antd";
+
+// import the following components from 'antd'
+import { Button, Card, Checkbox, Form, Input, List, Select } from "antd";
+
+// import the API function that will talk to back-end
+import { createTodo, getTodos, updateTodo, deleteTodo } from "../services/api";
+
+// import the `useNavigate` component from 'react-router-dom'
 import { useNavigate } from "react-router-dom";
+
+// import the styling the for our homepage component
+// import "./Homepage.css";
 
 function Homepage() {
   // get the `logout` function
   const { logout } = useAuth();
+
   // variable that is going to allow us to navigate to other pages
   const navigate = useNavigate();
 
