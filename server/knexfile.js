1d1c8671b492219c8c69a75f0d18665b1beb8cc1
Sunhaloo <azmaan151003@gmail.com>
2025-11-21 11:09:03 +0400

    Update: Arrived at checking if `knex.js` file is correctly configured!

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,10 +1,29 @@
+// knexfile.js
+require("dotenv").config();
+
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./db/app.db",
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
     },
-    useNullAsDefault: true,
+    migrations: {
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
+  },
+
+  production: {
+    client: "postgresql",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
     migrations: {
       directory: "./db/migrations",
     },
