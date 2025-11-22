d163db7e6f53dbd6fbc0bc7ac4ffd8db1233c611
Sunhaloo <azmaan151003@gmail.com>
2025-11-22 12:02:54 +0400

    Update: Removed trailing `/` character in front-end URL

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://first-todo-react-dstv.vercel.app/",
+      "https://first-todo-react-dstv.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
